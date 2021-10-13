962a9b0679afe43466bb0a61879b7edd7c14b4f4
KenTran801 <utes.fan.tran@gmail.com>
2021-10-12 18:51:41 -0600

    started some pseudo coding on the js.file

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create Variables
+
+// Created Objects - containing the time and assign a value
+
+// Create for loop that generates the timeblocks (rows, hour column, text input column, and button column)
+
