180ae7f561edcba23e14e256b36f2b37f07ea9d5
KenTran801 <utes.fan.tran@gmail.com>
2021-10-15 16:45:23 -0600

    Added time variables and dislayed current date on page using moment

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
 // Create Variables
-
+var currentDayElement = moment().format('MMMM Do YYYY');
 // Created Objects - containing the time and assign a value
+var calendlyTimes = [
+    {
+      normalTime: "8:00 AM",
+    },
+  
+    {
+      normalTime: "9:00 AM",
+    },
+  
+    {
+      normalTime: "10:00 AM",
+    },
+  
+    {
+      normalTime: "11:00 AM",
+    },
+  
+    {
+      normalTime: "12:00 PM",
+
+    },
+  
+    {
+      normalTime: "1:00 PM",
+    },
+  
+    {
+      normalTime: "2:00 PM",
+    },
+  
+    {
+      normalTime: "3:00 PM",
+    },
+  
+    {
+      normalTime: "4:00 PM",
+    },
+  
+    {
+      normalTime: "5:00 PM",
+    },
+  
+    {
+      normalTime: "6:00 PM",
+    },
+  
+    {
+      normalTime: "7:00 PM",
+    },
+  
+    {
+      normalTime: "8:00 PM",
+    },
+  
+    {
+      normalTime: "9:00 PM",
+    },
+  ];
+
+// Diaplsy current date
+$("#currentDay").append(currentDayElement);
 
 // Create for loop that generates the timeblocks (rows, hour column, text input column, and button column)
 
